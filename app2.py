a42b5bb55ff0325d18dd9a6d7156637f92cc87ba
akresniak <szwedlab@MacBook-Pro-szwedlab.local>
2019-09-10 23:21:07 +0200

    working version with better sliders
    no callbacks

--- app2.py
+++ app2.py
@@ -23,13 +23,16 @@ flows, edge_colors, edges = c_d.get_flow_difference(dset, 1, 2)
 suppliers_mask, receivers_mask, rest_mask, supplier_nodes, receiver_nodes = \
     c_d.get_supplier_receiver_mask(dset, 1, 2, N)
 
-node_edge_colors = np.array(['black'] * N)
-node_edge_colors[suppliers_mask[0]] = 'y'
-node_edge_colors[receivers_mask[0]] = 'coral'
+node_edge_colors = np.array(['darkslategrey'] * N)
+node_edge_colors[suppliers_mask[0]] = 'darkgreen'
+node_edge_colors[receivers_mask[0]] = 'darkred'
 
-node_colors = np.array(['white'] * N)
-node_colors[suppliers_mask[1]] = 'green'
-node_colors[receivers_mask[0]] = 'coral'
+node_colors = np.array(['whitesmoke'] * N)
+node_colors[suppliers_mask[1]] = 'lightgreen'
+node_colors[receivers_mask[1]] = 'coral'
+
+node_types = np.array(['receivers'] * N)
+node_types[suppliers_mask[1]] = 'suppliers'
 
 G = nx.DiGraph()
 G.add_nodes_from(node_list)
@@ -65,38 +68,51 @@ edge_trace = go.Scatter(
 
 node_x = []
 node_y = []
-for node in G.nodes():
+
+for i, node in enumerate(G.nodes()):
     x, y = xy[node]
     node_x.append(x)
     node_y.append(y)
 
 node_trace = go.Scatter(
-    x=node_x, y=node_y, 
+    x=node_x, 
+    y=node_y, 
     mode='markers+text',
     text=tuple(node_list),
-    textposition="top right",
+    textposition='top right',
     hoverinfo='text',
     marker=dict(
         showscale=False,
         colorscale='YlGnBu',
         reversescale=True,
         color=node_colors,
-        size=10,
-        line_width=4))
+        size=15,
+        line=dict(width=4,color=tuple(node_edge_colors))
+        ))
+
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.Div([
-        html.H1("Network Graph")
+    html.H1("Network Graph")
 
     ], style={
         'textAlign': "center"
     }),
+    html.Div([html.H3("Slide to set hours for Hour_2 - Hour_1 network visualisation", style={"text-align": "center", 'padding': 10}, className="row"),
+              dcc.Slider(id="nodes", min=1, max=len(dset), value=1, step=1, updatemode="drag",
+                         marks={i: 'Hour_1: ' if i == 0 else str(i) for i in range(0, len(dset)+1)}, className="row")],
+             style={"display": "block", "margin-left": "auto", "margin-right": "auto", "width": "40%", "padding": 5}),
+html.Div([html.Span("", style={"text-align": "center", 'padding': 10}, className="row"),             
+             dcc.Slider(id="nodes2", min=1, max=len(dset), value=2, step=1, updatemode="drag",
+                         marks={i: 'Hour_2: ' if i == 0 else str(i) for i in range(0, len(dset)+1)}, className="row")],
+             style={"display": "block", "margin-left": "auto", "margin-right": "auto", "width": "40%", "padding": 30}),
+
     html.Div([
-        dcc.Graph(id="my-graph",
+        dcc.Graph(id="red-graph",
                   figure={
-                      "data": [edge_trace, node_trace],
+                      "data": [edge_trace,node_trace],
                       "layout": go.Layout(
                           title='Network Graph',
                           titlefont={'size': 16},
@@ -107,25 +123,9 @@ app.layout = html.Div([
                                  'zeroline': False, 'showticklabels': False},
                           yaxis={'showgrid': False,
                                  'zeroline': False, 'showticklabels': False})
-                  }
+                  })
+            ])
 
-                  ),
-                  
-                  ]),
-html.Label('Network difference: Hour2 - Hour1'),
-    dcc.Slider(
-        min=1,
-        max=len(dset),
-        marks={i: 'Hour 1' if i == 1 else str(i) for i in range(1, len(dset))},
-        value=5,
-    ),
-
-    dcc.Slider(
-        min=1,
-        max=len(dset),
-        marks={i: 'Hour 2' if i == 1 else str(i) for i in range(1, len(dset))},
-        value=6,
-    )
 ], className="container")
 
 #server = app.server
